5556c0583e1de3fae8363e1b280dced92f516dfd
Melky <melky12ricardo@gmail.com>
2026-07-04 21:00:43 -0400

    Se ha creado los DTOs necesarios para recetas, tambien se ha implemendo AutoMapper para la conversion entre el modelo y los DTOs y por ultimo en la tabla CookingSteps la columna Description ahora es Instruction

---
+++ Core/DTOs/RecipeDTOs.cs
@@ -0,0 +1,49 @@
+﻿using Core.Models;
+using static Core.Models.Enums.ModelEnums;
+
+namespace Core.DTOs
+{
+    public class RecipeDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string ImageUrl { get; set; } = string.Empty;
+        public TimeSpan PreparationTime { get; set; }
+        public TimeSpan CookingTime { get; set; }
+        public int Servings { get; set; }
+        public RecipeDifficulty Difficulty { get; set; }
+        public int Calories { get; set; }
+        public bool IsPublic { get; set; }
+        public ICollection<Recipe_Category> Recipe_Categories { get; set; } = [];
+    }
+
+    public class CreateRecipeDTO
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string ImageUrl { get; set; } = string.Empty;
+        public TimeSpan PreparationTime { get; set; }
+        public TimeSpan CookingTime { get; set; }
+        public int Servings { get; set; }
+        public RecipeDifficulty Difficulty { get; set; }
+        public int Calories { get; set; }
+        public int UserId { get; set; }
+        public bool IsPublic { get; set; } = false;
+        public ICollection<CookingStep> CookingSteps { get; set; } = [];
+        public ICollection<Tip> Tips { get; set; } = [];
+    }
+
+    public class UpdateRecipeDTO
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string ImageUrl { get; set; } = string.Empty;
+        public TimeSpan PreparationTime { get; set; }
+        public TimeSpan CookingTime { get; set; }
+        public int Servings { get; set; }
+        public RecipeDifficulty Difficulty { get; set; }
+        public int Calories { get; set; }
+        public bool IsPublic { get; set; }
+    }
+}
