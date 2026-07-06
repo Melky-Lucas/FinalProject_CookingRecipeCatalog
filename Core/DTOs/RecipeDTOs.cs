68f0b8925112993bdbf0a441060de1115c6a68ae
Melky <melky12ricardo@gmail.com>
2026-07-06 10:32:31 -0400

    Se han creado los DTOs base para las instrucciones de las recetas y se ha resuelto un problema con el mapeo del controlador de recetas.

--- Core/DTOs/RecipeDTOs.cs
+++ Core/DTOs/RecipeDTOs.cs
@@ -8,42 +8,52 @@ namespace Core.DTOs
         public int Id { get; set; }
         public string Title { get; set; } = null!;
         public string Description { get; set; } = null!;
-        public string ImageUrl { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = null!;
         public TimeSpan PreparationTime { get; set; }
         public TimeSpan CookingTime { get; set; }
         public int Servings { get; set; }
         public RecipeDifficulty Difficulty { get; set; }
         public int Calories { get; set; }
         public bool IsPublic { get; set; }
-        public ICollection<Recipe_Category> Recipe_Categories { get; set; } = [];
+        public ICollection<Recipe_Ingredient> Recipe_Ingredients { get; set; } = [];
+        public ICollection<RecipeCategory> Categories { get; set; } = [];
+        public ICollection<RecipeCookingStepDTO> CookingSteps { get; set; } = [];
+        public ICollection<Tip> Tips { get; set; } = [];
+        public User User { get; set; } = null!;
+
     }
 
     public class CreateRecipeDTO
     {
         public string Title { get; set; } = null!;
         public string Description { get; set; } = null!;
-        public string ImageUrl { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = null!;
         public TimeSpan PreparationTime { get; set; }
         public TimeSpan CookingTime { get; set; }
         public int Servings { get; set; }
         public RecipeDifficulty Difficulty { get; set; }
         public int Calories { get; set; }
         public int UserId { get; set; }
-        public bool IsPublic { get; set; } = false;
-        public ICollection<CookingStep> CookingSteps { get; set; } = [];
+        public bool IsPublic { get; set; }
+        public int[] Category_Ids { get; set; } = [];
+        public ICollection<Recipe_Ingredient> Recipe_Ingredients { get; set; } = [];
+        public ICollection<CreateCookingStepDTO> CookingSteps { get; set; } = [];
         public ICollection<Tip> Tips { get; set; } = [];
     }
 
     public class UpdateRecipeDTO
     {
+        public int Id { get; set; }
         public string Title { get; set; } = null!;
         public string Description { get; set; } = null!;
-        public string ImageUrl { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = null!;
         public TimeSpan PreparationTime { get; set; }
         public TimeSpan CookingTime { get; set; }
         public int Servings { get; set; }
         public RecipeDifficulty Difficulty { get; set; }
         public int Calories { get; set; }
         public bool IsPublic { get; set; }
+        public int[] Category_Ids { get; set; } = [];
+        public ICollection<Recipe_Ingredient> Recipe_Ingredients { get; set; } = [];
     }
 }
