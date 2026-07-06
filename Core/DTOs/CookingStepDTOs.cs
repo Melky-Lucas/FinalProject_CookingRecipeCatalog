dff751874a5f1e20434002886669303b8c8b4894
Melky <melky12ricardo@gmail.com>
2026-07-06 10:54:24 -0400

    Se han creado DTOs base para los consejos del 'chef' o admins del sistema.

--- Core/DTOs/CookingStepDTOs.cs
+++ Core/DTOs/CookingStepDTOs.cs
@@ -2,6 +2,7 @@
 {
     public class RecipeCookingStepDTO
     {
+        public int Id { get; set; }
         public int StepNumber { get; set; }
         public string Title { get; set; } = null!;
         public TimeSpan EstimatedDuration { get; set; }
---
+++ Core/DTOs/TipDTOs.cs
@@ -0,0 +1,24 @@
+﻿namespace Core.DTOs
+{
+    public class RecipeTipDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Content { get; set; } = null!;
+    }
+
+    public class CreateTipDTO
+    {
+        public int RecipeId { get; set; }
+        public int UserId { get; set; }
+        public string Content { get; set; } = null!;
+    }
+
+    public class UpdateTipDTO
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = null!;
+    }
+
+
+}
