316b37dcbd48ddcf637db7cd893337447a3f60f5
Melky <melky12ricardo@gmail.com>
2026-07-06 11:08:24 -0400

    Se implementaran DTOs para la tabla puente entre recetas e ingredientes junto con 2 mas, uno para ingredientes y otro para las unidades de medida.

---
+++ Core/DTOs/IngredientDTOs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DTOs
+{
+    public class IngredientDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public string CategoryName { get; set; } = null!;
+    }
+}
---
+++ Core/DTOs/MeasureUnitDTOs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DTOs
+{
+    public class MeasureUnitDTO
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string Abbreviation { get; set; } = null!;
+    }
+}
