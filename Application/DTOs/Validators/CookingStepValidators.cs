c42b6d136ca711242fad48de837b074a3c090ed3
Melky <melky12ricardo@gmail.com>
2026-07-12 16:40:59 -0400

    Se ha utilizado FluentValidation para asegurar que los datos cumplan con los requisitos.

---
+++ Application/DTOs/Validators/CookingStepValidators.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+
+namespace Application.DTOs.Validators
+{
+    public class CreateCookingStepDTOValidator : AbstractValidator<CreateCookingStepDTO>
+    {
+        public CreateCookingStepDTOValidator()
+        {
+            RuleFor(x => x.StepNumber)
+                .GreaterThan(0).WithMessage("Step number must be greater than zero.");
+
+            RuleFor(x => x.RecipeId)
+                .GreaterThan(0).WithMessage("RecipeId must be greater than zero.");
+
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(100).WithMessage("Title cannot exceed 100 characters.");
+
+            RuleFor(x => x.EstimatedDuration)
+                .GreaterThan(TimeSpan.Zero).WithMessage("Estimated duration must be greater than zero.");
+
+            RuleFor(x => x.Instruction)
+                .NotEmpty().WithMessage("Instruction is required.")
+                .MaximumLength(250).WithMessage("Instruction cannot exceed 250 characters.");
+        }
+    }
+
+    public class UpdateCookingStepDTOValidator : AbstractValidator<UpdateCookingStepDTO>
+    {
+        public UpdateCookingStepDTOValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("Id must be greater than zero.");
+
+            RuleFor(x => x.StepNumber)
+                .GreaterThan(0).WithMessage("Step number must be greater than zero.");
+
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(100).WithMessage("Title cannot exceed 100 characters.");
+
+            RuleFor(x => x.EstimatedDuration)
+                .GreaterThan(TimeSpan.Zero).WithMessage("Estimated duration must be greater than zero.");
+
+            RuleFor(x => x.Instruction)
+                .NotEmpty().WithMessage("Instruction is required.")
+                .MaximumLength(250).WithMessage("Instruction cannot exceed 250 characters.");
+        }
+    }
+}
---
+++ Application/DTOs/Validators/Recipe_IngredientValidators.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+
+namespace Application.DTOs.Validators
+{
+    public class CreateRecipe_IngredientDTOValidator : AbstractValidator<CreateRecipe_IngredientDTO>
+    {
+        public CreateRecipe_IngredientDTOValidator()
+        {
+            RuleFor(x => x.RecipeId)
+                .GreaterThan(0).WithMessage("RecipeId must be greater than zero.");
+
+            RuleFor(x => x.IngredientId)
+                .GreaterThan(0).WithMessage("IngredientId must be greater than zero.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0).WithMessage("Quantity must be greater than zero.");
+
+            RuleFor(x => x.UnitId)
+                .GreaterThan(0).WithMessage("UnitId must be greater than zero.");
+        }
+    }
+
+    public class UpdateRecipe_IngredientDTOValidator : AbstractValidator<UpdateRecipe_IngredientDTO>
+    {
+        public UpdateRecipe_IngredientDTOValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("Id must be greater than zero.");
+
+            RuleFor(x => x.RecipeId)
+                .GreaterThan(0).WithMessage("RecipeId must be greater than zero.");
+
+            RuleFor(x => x.IngredientId)
+                .GreaterThan(0).WithMessage("IngredientId must be greater than zero.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0).WithMessage("Quantity must be greater than zero.");
+
+            RuleFor(x => x.UnitId)
+                .GreaterThan(0).WithMessage("UnitId must be greater than zero.");
+        }
+    }
+}
