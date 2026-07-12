e6519fe026fa8f6d4dd4d01ac897c9a2b5dcb0ef
Melky <melky12ricardo@gmail.com>
2026-07-12 02:05:17 -0400

    Se han implementado las servicios en Services y se cambiado el namespace de los DTOs para que esten acorde a la estructura de carpetas.

--- Application/DTOs/IngredientDTOs.cs
+++ Application/DTOs/IngredientDTOs.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Core.DTOs
+namespace Application.DTOs
 {
     public class IngredientDTO
     {
---
+++ Application/Services/CookingStepService.cs
@@ -0,0 +1,19 @@
+﻿using Application.Base;
+using Application.Contract;
+using Application.DTOs;
+using Core.Interfaces;
+using Core.Interfaces.Repositories.Generic;
+using Core.Models;
+
+namespace Application.Services
+{
+    public class CookingStepService : BaseService<CookingStep, RecipeCookingStepDTO, CreateCookingStepDTO, UpdateCookingStepDTO>, ICookingStepService
+    {
+        protected override IGenericRepository<CookingStep> Repository => _unitOfWork.CookingSteps;
+        public CookingStepService(IUnitOfWork unitOfWork, IObjectMapper objectMapper)
+            : base(unitOfWork, objectMapper)
+        {
+        }
+    {
+    }
+}
