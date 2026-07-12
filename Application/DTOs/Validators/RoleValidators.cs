21178b814a09ee9bf35a13b6b5af96ebc358d328
Melky <melky12ricardo@gmail.com>
2026-07-12 16:40:59 -0400

    Se ha utilizado FluentValidation para asegurar que los datos cumplan con los requisitos.

---
+++ Application/DTOs/Validators/RoleValidators.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace Application.DTOs.Validators
+{
+    public class CreateRoleDTOValidator : AbstractValidator<CreateRoleDTO>
+    {
+        public CreateRoleDTOValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(50).WithMessage("Name cannot exceed 50 characters.");
+        }
+    }
+
+    public class UpdateRoleDTOValidator : AbstractValidator<UpdateRoleDTO>
+    {
+        public UpdateRoleDTOValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("Id must be greater than zero.");
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(50).WithMessage("Name cannot exceed 50 characters.");
+        }
+    }
+}
---
+++ Application/DTOs/Validators/UserValidators.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+
+namespace Application.DTOs.Validators
+{
+    public class CreateUserDTOValidator : AbstractValidator<RegisterUserDTO>
+    {
+        public CreateUserDTOValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty().WithMessage("Username is required.")
+                .MaximumLength(50).WithMessage("Username cannot exceed 50 characters.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.")
+                .MaximumLength(100).WithMessage("Email cannot exceed 100 characters.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters long.")
+                .MaximumLength(16).WithMessage("Password cannot exceed 16 characters.");
+
+            RuleFor(x => x.RoleId)
+                .GreaterThan(0).WithMessage("RoleId must be greater than zero.");
+        }
+    }
+
+    public class UpdateUserDTOValidator : AbstractValidator<UpdateProfileDTO>
+    {
+        public UpdateUserDTOValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty().WithMessage("Username is required.")
+                .MaximumLength(50).WithMessage("Username cannot exceed 50 characters.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.")
+                .MaximumLength(100).WithMessage("Email cannot exceed 100 characters.");
+
+            RuleFor(x => x.RoleId)
+                .GreaterThan(0).WithMessage("RoleId must be greater than zero.");
+        }
+    }
+}
