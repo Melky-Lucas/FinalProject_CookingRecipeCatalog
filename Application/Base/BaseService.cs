3a43c6e90be38904fac3b6c7eabc0e4339cee9e5
Melky <melky12ricardo@gmail.com>
2026-07-13 14:11:55 -0400

    FluentValidation se ha configurado en el BaseService y se ha creado una AppValidationExcetion para capturar los errores.

--- Application/Base/BaseService.cs
+++ Application/Base/BaseService.cs
@@ -1,6 +1,9 @@
-﻿using Core.Base;
+﻿using Application.Exceptions;
+using Core.Base;
 using Core.Interfaces;
 using Core.Interfaces.Repositories.Generic;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Application.Base
 {
@@ -10,12 +13,14 @@ namespace Application.Base
     {
         protected readonly IUnitOfWork _unitOfWork;
         protected readonly IObjectMapper _mapper;
+        protected readonly IServiceProvider _serviceProvider;
         protected abstract IGenericRepository<TEntity> Repository { get; }
 
-        protected BaseService(IUnitOfWork unitOfWork, IObjectMapper mapper)
+        protected BaseService(IUnitOfWork unitOfWork, IObjectMapper mapper, IServiceProvider serviceProvider)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _serviceProvider = serviceProvider;
         }
 
         public virtual async Task<ServiceResult<IEnumerable<TDto>>> GetAllAsync()
@@ -36,6 +41,8 @@ namespace Application.Base
 
         public virtual async Task<ServiceResult<TDto>> CreateAsync(TCreateDto dto)
         {
+            await ValidateAsync(dto);
+
             var entity = _mapper.Map<TCreateDto, TEntity>(dto);
             Repository.Add(entity);
             await _unitOfWork.SaveChangesAsync();
@@ -45,11 +52,14 @@ namespace Application.Base
 
         public virtual async Task<ServiceResult<TDto>> UpdateAsync(int id, TUpdateDto dto)
         {
+            await ValidateAsync(dto);
+
             var entity = await Repository.GetByIdAsync(id);
             if (entity is null)
                 return ServiceResult<TDto>.Failure("Entidad no encontrada", 404);
             Repository.Update(entity);
             await _unitOfWork.SaveChangesAsync();
+
             return ServiceResult<TDto>.Success(_mapper.Map<TEntity, TDto>(entity));
         }
 
@@ -60,7 +70,23 @@ namespace Application.Base
                 return ServiceResult.Failure("Entidad no encontrada", 404);
             Repository.Delete(entity);
             await _unitOfWork.SaveChangesAsync();
+
             return ServiceResult.Success();
+        }
+
+        protected async Task ValidateAsync<TCUDto>(TCUDto dto)
+        {
+            var validator = _serviceProvider.GetService<IValidator<TCUDto>>();
+
+            if (validator == null)
+                return;
+            
+            var validationResult = await validator.ValidateAsync(dto);
+
+            if (!validationResult.IsValid)
+            {
+                throw new AppValidationException(validationResult.Errors);
+            }
         }
     }
 }
