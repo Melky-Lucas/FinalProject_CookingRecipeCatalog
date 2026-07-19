aa26d8a5d0cc5175994a1e6fd7ce40e51b45b866
Melky <melky12ricardo@gmail.com>
2026-07-19 02:10:50 -0400

    PasswordHasher implementado y configurado.

--- Application/Services/UserService.cs
+++ Application/Services/UserService.cs
@@ -10,11 +10,24 @@ namespace Application.Services
 {
     public class UserService : BaseService<User, ProfileDTO, RegisterUserDTO, UpdateProfileDTO>, IUserService
     {
+        private readonly IPasswordHasher _passwordHasher;
         protected override IGenericRepository<User> Repository => _unitOfWork.Users;
-        public UserService(IUnitOfWork unitOfWork, IObjectMapper objectMapper, IServiceProvider serviceProvider)
+        public UserService(IUnitOfWork unitOfWork, IObjectMapper objectMapper, IServiceProvider serviceProvider, IPasswordHasher passwordHasher)
             : base(unitOfWork, objectMapper, serviceProvider)
         {
+            _passwordHasher = passwordHasher;
+        }
 
+        public override async Task<ServiceResult<ProfileDTO>> CreateAsync(RegisterUserDTO userDTO)
+        {
+            var hashedPassword = _passwordHasher.Hash(userDTO.Password);
+            var user = _mapper.Map<RegisterUserDTO, User>(userDTO);
+            user.Password.PasswordHash = hashedPassword;
+
+            Repository.Add(user);
+            await _unitOfWork.SaveChangesAsync();
+
+            return ServiceResult<ProfileDTO>.Success(_mapper.Map<User, ProfileDTO>(user), "User created successfully", 201);
         }
     }
 }
